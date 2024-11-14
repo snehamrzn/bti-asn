7f8cea9b165115dcef8225575b9701803b533cf8
Sneha Maharjan <snehamrzzn@gmail.com>
2024-11-14 16:58:43 -0500

    Final commit

--- modules/projects.js
+++ modules/projects.js
@@ -1,59 +1,110 @@
-const projectData = require("../data/projectData");
-const sectorData = require("../data/sectorData");
+require('dotenv').config();
+require('pg');
+const Sequelize = require('sequelize');
 
-let projects =[];
+// set up sequelize to point to our postgres database
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+  host: process.env.PGHOST,
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
+});
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
+  });
+
+  const Sector = sequelize.define(
+    "Sector",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      sector_name: Sequelize.STRING,
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+
+  const Project = sequelize.define(
+    "Project",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: Sequelize.STRING,
+      feature_img_url: Sequelize.STRING,
+      summary_short: Sequelize.TEXT,
+      intro_short: Sequelize.TEXT,
+      impact: Sequelize.TEXT,
+      original_source_url: Sequelize.STRING,
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+
+  Project.belongsTo(Sector, {foreignKey: 'sector_id'});
 
 function Initialize()
 {
-    return new Promise((resolve,reject) => {
+    return new Promise(async (resolve,reject) => {
         try{
-            projectData.forEach((element)=>{
-                let sectorMatch = sectorData.find(sector => sector.id === element.sector_id );
-        
-                let objCopy = {...element, 
-                               sector: sectorMatch ? sectorMatch.sector_name : null
-                };
-        
-                projects.push(objCopy);
-              //  console.log(objCopy);
-                resolve();
-            })     
+          sequelize.sync();
+          console.log('Database synchronized successfully.');
+          resolve();     
         }catch(error){
-            reject(`error:${error.message}`);
+            reject('Database synchronization failed.');
         }
-      
-
     });
 
 }
 
 function getAllProject()
 {
-    return new Promise((resolve,reject) => {
-        if(projects.length >0)
-        {
+    return new Promise(async(resolve,reject) => {
+        try{
+            const projects = await Project.findAll({
+                include: [Sector],
+            });
             resolve(projects);
-        }else{
-            reject("unable to find requested project")
-        }
-        
+        }catch(error){
+            reject(error);
+        }  
     });
-    
-
 }
 
 function getProjectById(projectId)
 {
-    return new Promise((resolve,reject) =>
+    return new Promise(async (resolve,reject) =>
     {
-        
-        let obj = projects.find(project => project.id === projectId);
-        if(obj)
-        {
-            resolve(obj);
-        }else{
-            reject("unable to find requested project");
+        try{
+            const projects = await Project.findAll(
+                {
+                include: [Sector],
+                where: { id: projectId},
+            });
+            if (projects.length > 0) {
+                resolve(projects[0]);
+            } else {
+                reject("Unable to find requested project.");
+            }
+        }catch(error){
+            reject("Unable to find requested project.")
         }
         
     });
@@ -62,21 +113,88 @@ function getProjectById(projectId)
 
 function getProjectBySector(sector)
 {
-    return new Promise((resolve, reject) =>
+    return new Promise(async (resolve, reject) =>
     {
-         let objSector = projects.filter((project) => project.sector.toLowerCase().includes(sector.toLowerCase()));
-         if(objSector.length > 0)
-         {
-             resolve(objSector) ;
-         } else{
-             reject("unable to find requested project");
-         }
-        
+        try{
+            const projectSector = await Project.findAll({include: [Sector], where: {
+                '$Sector.sector_name$': {
+                [Sequelize.Op.iLike]: `%${sector}%`
+                }
+                }});
+            resolve(projectSector);
+        }catch{
+            reject("Unable to find requested projects");
+        }    
+    });
+
+}
+function addProject(projectData)
+{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            const newProject = await Project.create(projectData);
+            resolve(newProject);
+        }catch(error)
+        {
+            reject(error.errors[0].message);
+        }
+    });
+}
+function getAllSectors()
+{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            const sector = await Sector.findAll();
+            resolve (sector);
+        }catch(error){
+            reject(error);
+        }
+    });
+
+}
+function editProject(id, projectData)
+{
+    return new Promise(async (resolve, reject) =>{
+       try{
+        const edit = await Project.update(projectData, {
+            where: { id: id },
+          });
+        resolve(edit);
+
+       }catch(error){
+            reject(error.errors[0].message);
+        }
+    });
+}
+
+function deleteProject(id)
+{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            const delProject = await Project.destroy(
+                {
+                    where: {id: id}
+                });
+                resolve(delProject);
+        }catch(error)
+        {
+            reject(error.errors[0].message);
+        }
+
     });
 
 }
 
 
-module.exports = {Initialize, getAllProject, getProjectById, getProjectBySector };
+module.exports = {
+  Initialize,
+  getAllProject,
+  getProjectById,
+  getProjectBySector,
+  addProject,
+  getAllSectors,
+  editProject,
+  deleteProject
+};
 
 
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /********************************************************************************
-* BTI325 – Assignment 02
+* BTI325 – Assignment 05
 *
 * I declare that this assignment is my own work in accordance with Seneca's
 * Academic Integrity Policy:
 *
 * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
 *
-* Name: _Sneha Maharjan_ Student ID: ___170814222____ Date: ___2024-10-8____
+* Name: _Sneha Maharjan_ Student ID: ___170814222____ Date: ___2024-11-14____
 *
-* Published url- bti-asn-3.vercel.app
+* Published url -
 ********************************************************************************/
 const projectData = require("./modules/projects");
 const express = require('express');
@@ -22,6 +22,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 
 app.set('views', __dirname + '/views');
+
+app.use(express.urlencoded({extended:true}));
 
 const main = async() =>{
     await projectData.Initialize();
@@ -61,9 +63,10 @@ const main = async() =>{
     });
 
     app.get('/solutions/projects/:id', async(req,res) =>{
-        try{
-            const id = parseInt(req.params.id); // returns a string, parsing to int
+        try{    
+            const id = parseInt(req.params.id); 
             let projectId = await projectData.getProjectById(id);
+            // console.log(projectId);
             res.render("project", {project: projectId});
         }catch(error){
             res.status(404).render("404", { message: "Unable to find requested project."});
@@ -71,12 +74,77 @@ const main = async() =>{
 
 
     });
+    
+    app.get('/solutions/addProject', async(req,res) =>{
+        try {
+            const sectorData = await projectData.getAllSectors(); 
+            res.render("addProject", { sectors: sectorData }); 
+            
+        } catch (err) {
+            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
+        }
 
+    });
+     
+    app.post('/solutions/addProject', async(req,res) =>{
+       
+       try{
+            const newProjectData =  req.body;
+            await projectData.addProject(newProjectData);
+            res.redirect("/solutions/projects");
+       }catch(error){
+        res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` })
+       }
+    });
+
+    app.get('/solutions/editProject/:id', async(req,res) =>
+    {
+        const projectId = parseInt(req.params.id);
+        try{
+
+            const  prjData = await projectData.getProjectById(projectId);
+
+            const  sectorData = await projectData.getAllSectors();
+
+            res.render("editProject", { sectors: sectorData, project: prjData});
+        }catch(err)
+        {
+            res.status(404).render("404", { message: err });
+        }
+
+    });
+
+    app.post('/solutions/editProject',async(req,res) =>{
+            const id =  parseInt(req.body.id);
+            const projData = req.body;
+        try{
+            await projectData.editProject(id,projData);
+            res.redirect("/solutions/projects");
+
+        }catch(error)
+        {
+            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${error}` });
+        }
+
+    });
+
+    app.get('/solutions/deleteProject/:id', async(req,res) => {
+        const delId = parseInt(req.params.id);
+        try
+        {
+            await projectData.deleteProject(delId);
+            res.redirect("/solutions/projects");
+
+        }catch(error)
+        {
+            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${error}` });
+        }
+    });
     // Other route handlers, middleware, etc ...
 
     app.use((req, res, next) => {
         // res.status(404).render("404"); 
-        res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+        res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for."});
     });
   
   // app.listen()

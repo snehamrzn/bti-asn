6feb4b8a8d5e5e832accfbfce8af655a925c8ac3
Sneha Maharjan <snehamrzzn@gmail.com>
2024-11-28 23:01:37 -0500

    Assignment 6

---
+++ modules/auth-service.js
@@ -0,0 +1,107 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const bcrypt = require('bcryptjs');
+
+let Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  email: String,
+  loginHistory: [
+    {
+      dateTime: Date,
+      userAgent: String,
+    },
+  ],
+  country: String,
+});
+
+let User;
+
+function Initialize() {
+  return new Promise(function (resolve, reject) {
+    let db = mongoose.createConnection(process.env.MONGODB);
+    db.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+
+      reject(err); // reject the promise with the provided error
+    });
+    db.once("open", () => {
+      console.log("MongoDB connection established");
+      User = db.model("users", userSchema);
+      resolve();
+    });
+  });
+}
+
+function registerUser(userData) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (userData.password != userData.password2) {
+        reject("Passwords do not match");
+      }
+      try {
+        const hash = await bcrypt.hash(userData.password, 10);
+        userData.password = hash;
+        let newUser = new User(userData);
+        await newUser.save();
+        resolve();
+      } catch (err) {
+        reject("There was an error encrypting the password");
+      }
+    } catch (err) {
+      if (err.code == 11000) {
+        reject("User Name already taken");
+      } else {
+        reject(`There was an error creating the user: ${err}`);
+      }
+    }
+  });
+}
+
+function checkUser(userData) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const users = await User.find({ userName: userData.userName });
+      if (users.length === 0) {
+        reject(`Unable to find user: ${userData.userName}`);
+      }
+
+      const Match = await bcrypt.compare(userData.password, users[0].password);
+      if(!Match)
+      {
+        reject(`Incorrect Password for user: ${userData.userName}`)
+      }
+
+      if (users[0].loginHistory.length == 8) {
+        users[0].loginHistory.pop;
+      }
+
+
+      users[0].loginHistory.unshift({
+        dateTime: new Date().toString(),
+        userAgent: userData.userAgent,
+      });
+
+
+      try {
+        const updateResult = await User.updateOne(
+            { userName: users[0].userName },
+            { $set: { loginHistory: users[0].loginHistory } }
+          );
+      } catch(err)  {
+        reject(`There was an error verifying the user: ${err} `);
+      }
+      resolve(users[0]);
+      
+    } catch (err) {
+     reject(`Unable to find user: ${userData.userName}`)
+    }
+  });
+}
+module.exports = { Initialize, registerUser, checkUser };
--- server.js
+++ server.js
@@ -1,164 +1,246 @@
 /********************************************************************************
-* BTI325 – Assignment 05
-*
-* I declare that this assignment is my own work in accordance with Seneca's
-* Academic Integrity Policy:
-*
-* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
-*
-* Name: _Sneha Maharjan_ Student ID: ___170814222____ Date: ___2024-11-14____
-*
-* Published url -
-********************************************************************************/
+ * BTI325 – Assignment 06
+ *
+ * I declare that this assignment is my own work in accordance with Seneca's
+ * Academic Integrity Policy:
+ *
+ * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+ *
+ * Name: _Sneha Maharjan_ Student ID: ___170814222____ Date: ___2024-11-29____
+ *
+ * Published url - bti-asn-3.vercel.app
+ ********************************************************************************/
 const projectData = require("./modules/projects");
-const express = require('express');
+const authData = require("./modules/auth-service");
+
+const clientSessions = require("client-sessions");
+
+const express = require("express");
 const path = require("path");
 
 const app = express();
 const HTTP_PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(
+  clientSessions({
+    cookieName: "session",
+    secret: "o6LjQ5EVNC28ZgK64hDELM18ScpFQr",
+    duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+    activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
+  })
+);
 
-app.set('view engine', 'ejs');
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
 
-app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
 
-const main = async() =>{
+app.set("views", __dirname + "/views");
+
+app.use(express.urlencoded({ extended: true }));
+
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+}
+
+const main = async () => {
+  try {
     await projectData.Initialize();
+    await authData.Initialize();
 
-
-    app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
-    
-    app.get('/',(req, res) => {
-            // res.send('Assignment2:Student Name - Sneha Maharjan Student ID - 170814222');
-
-            res.render("home");
+    app.listen(HTTP_PORT, function () {
+      console.log(`app listening on: ${HTTP_PORT}`);
     });
+  } catch (err) {
+    console.log(`unable to start server ${err}`);
+  }
 
-    app.get('/about', (req, res) => {
-        res.render("about");
-    })
-   
-// 
+  app.get("/", (req, res) => {
+    // res.send('Assignment2:Student Name - Sneha Maharjan Student ID - 170814222');
 
-    app.get('/solutions/projects',async(req,res)=>{
-        const sector = req.query.sector; 
-       try{
-            // query filter params
-            
-            if(sector) {
-                let sectors = await projectData.getProjectBySector(sector);
-                // res.send(sectors);
-                res.render("projects", {projects: sectors});
-            } else {
-                let project = await projectData.getAllProject();
-                //res.send(await projectData.getAllProject());
-                res.render("projects", {projects: project});
-            }
-       }catch(error){
-        res.status(404).render("404", {message: `No projects found for the sector: ${sector}`});
-       }
+    res.render("home");
+  });
+
+  app.get("/about", (req, res) => {
+    res.render("about");
+  });
+
+  //
+
+  app.get("/solutions/projects", async (req, res) => {
+    const sector = req.query.sector;
+    try {
+      // query filter params
+
+      if (sector) {
+        let sectors = await projectData.getProjectBySector(sector);
+        // res.send(sectors);
+        res.render("projects", { projects: sectors });
+      } else {
+        let project = await projectData.getAllProject();
+        //res.send(await projectData.getAllProject());
+        res.render("projects", { projects: project });
+      }
+    } catch (error) {
+      res.status(404).render("404", {
+        message: `No projects found for the sector: ${sector}`,
+      });
+    }
+  });
+
+  app.get("/solutions/projects/:id", async (req, res) => {
+    try {
+      const id = parseInt(req.params.id);
+      let projectId = await projectData.getProjectById(id);
+      // console.log(projectId);
+      res.render("project", { project: projectId });
+    } catch (error) {
+      res
+        .status(404)
+        .render("404", { message: "Unable to find requested project." });
+    }
+  });
+
+  app.get("/solutions/addProject", ensureLogin, async (req, res) => {
+    try {
+      const sectorData = await projectData.getAllSectors();
+      res.render("addProject", { sectors: sectorData });
+    } catch (err) {
+      res.render("500", {
+        message: `I'm sorry, but we have encountered the following error: ${err}`,
+      });
+    }
+  });
+
+  app.post("/solutions/addProject", ensureLogin, async (req, res) => {
+    try {
+      const newProjectData = req.body;
+      await projectData.addProject(newProjectData);
+      res.redirect("/solutions/projects");
+    } catch (error) {
+      res.render("500", {
+        message: `I'm sorry, but we have encountered the following error: ${error}`,
+      });
+    }
+  });
+
+  app.get("/solutions/editProject/:id", ensureLogin, async (req, res) => {
+    const projectId = parseInt(req.params.id);
+    try {
+      const prjData = await projectData.getProjectById(projectId);
+
+      const sectorData = await projectData.getAllSectors();
+
+      res.render("editProject", { sectors: sectorData, project: prjData });
+    } catch (err) {
+      res.status(404).render("404", { message: err });
+    }
+  });
+
+  app.post("/solutions/editProject", ensureLogin, async (req, res) => {
+    const id = parseInt(req.body.id);
+    const projData = req.body;
+    try {
+      await projectData.editProject(id, projData);
+      res.redirect("/solutions/projects");
+    } catch (error) {
+      res.render("500", {
+        message: `I'm sorry, but we have encountered the following error: ${error}`,
+      });
+    }
+  });
+
+  app.get("/solutions/deleteProject/:id", ensureLogin, async (req, res) => {
+    const delId = parseInt(req.params.id);
+    try {
+      await projectData.deleteProject(delId);
+      res.redirect("/solutions/projects");
+    } catch (error) {
+      res.render("500", {
+        message: `I'm sorry, but we have encountered the following error: ${error}`,
+      });
+    }
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login", { errorMessage: "", userName: "" });
+  });
+
+  app.get("/register", (req, res) => {
+    res.render("register", {
+      errorMessage: "",
+      successMessage: "",
+      userName: "",
     });
+  });
 
-    app.get('/solutions/projects/:id', async(req,res) =>{
-        try{    
-            const id = parseInt(req.params.id); 
-            let projectId = await projectData.getProjectById(id);
-            // console.log(projectId);
-            res.render("project", {project: projectId});
-        }catch(error){
-            res.status(404).render("404", { message: "Unable to find requested project."});
-        }
+  app.post("/register", async (req, res) => {
+    const userData = req.body;
+    try {
+      await authData.registerUser(userData);
+      res.render("register", {
+        errorMessage: "",
+        successMessage: "User created",
+        userName: "",
+      });
+    } catch (err) {
+      res.render("register", {
+        errorMessage: err,
+        successMessage: "",
+        userName: req.body.userName,
+      });
+    }
+  });
 
+  app.post("/login", async (req, res) => {
+    req.body.userAgent = req.get("User-Agent");
+    try {
+      let user = await authData.checkUser(req.body);
+      req.session.user = {
+        userName: user.userName, // authenticated user's userName
+        email: user.email, // authenticated user's email
+        loginHistory: user.loginHistory, // authenticated user's loginHistory
+      };
+      res.redirect("/solutions/projects");
+    } catch (err) {
+      res.render("login", { errorMessage: err, userName: req.body.userName });
+    }
+  });
 
+  app.get("/logout", (req, res) => {
+    req.session.reset();
+    res.redirect("/");
+  });
+
+  app.get("/userHistory", ensureLogin, (req, res) => {
+    res.render("userHistory");
+  });
+  // Other route handlers, middleware, etc ...
+
+  app.use((req, res, next) => {
+    // res.status(404).render("404");
+    res.status(404).render("404", {
+      message: "I'm sorry, we're unable to find what you're looking for.",
     });
+  });
 
-    
-    app.get('/solutions/addProject', async(req,res) =>{
-        try {
-            const sectorData = await projectData.getAllSectors(); 
-            res.render("addProject", { sectors: sectorData }); 
-            
-        } catch (err){
-            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
-        }
-
-    });
-    
-     
-    app.post('/solutions/addProject', async(req,res) =>{
-       
-       try{
-            const newProjectData = req.body;
-            await projectData.addProject(newProjectData);
-            res.redirect("/solutions/projects");
-       }catch(error){
-        res.render("500", { message: `I'm sorry, but we have encountered the following error: ${error}` })
-       }
-    });
-
-    app.get('/solutions/editProject/:id', async(req,res) =>
-    {
-        const projectId = parseInt(req.params.id);
-        try{
-
-            const  prjData = await projectData.getProjectById(projectId);
-
-            const  sectorData = await projectData.getAllSectors();
-
-            res.render("editProject", { sectors: sectorData, project: prjData});
-        }catch(err)
-        {
-            res.status(404).render("404", { message: err });
-        }
-
-    });
-
-    app.post('/solutions/editProject',async(req,res) =>{
-            const id =  parseInt(req.body.id);
-            const projData = req.body;
-        try{
-            await projectData.editProject(id,projData);
-            res.redirect("/solutions/projects");
-
-        }catch(error)
-        {
-            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${error}` });
-        }
-
-    });
-
-    app.get('/solutions/deleteProject/:id', async(req,res) => {
-        const delId = parseInt(req.params.id);
-        try
-        {
-            await projectData.deleteProject(delId);
-            res.redirect("/solutions/projects");
-
-        }catch(error)
-        {
-            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${error}` });
-        }
-    });
-    // Other route handlers, middleware, etc ...
-
-    app.use((req, res, next) => {
-        // res.status(404).render("404"); 
-        res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for."});
-    });
-  
   // app.listen()
 
-    // app.get('/solutions/projects/sector-demo', async(req,res) =>{
-    //     try{
-    //         res.send(await projectData.getProjectBySector('ind'));
-    //     }catch(error)
-    //     {
-    //         res.send(error);         
-    //     }
-    // });
+  // app.get('/solutions/projects/sector-demo', async(req,res) =>{
+  //     try{
+  //         res.send(await projectData.getProjectBySector('ind'));
+  //     }catch(error)
+  //     {
+  //         res.send(error);
+  //     }
+  // });
 };
 main();
-

8d8a6606bcea882965956f8a9ad0e7e087e89b6b
Sneha Maharjan <snehamrzzn@gmail.com>
2024-11-28 23:27:08 -0500

    Assignment 6-encrypt

--- modules/auth-service.js
+++ modules/auth-service.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 let Schema = mongoose.Schema;
 
@@ -41,19 +41,19 @@ function Initialize() {
 
 function registerUser(userData) {
   return new Promise(async (resolve, reject) => {
+    if (userData.password != userData.password2) {
+      reject("Passwords do not match");
+    }
     try {
-      if (userData.password != userData.password2) {
-        reject("Passwords do not match");
-      }
-      try {
-        const hash = await bcrypt.hash(userData.password, 10);
-        userData.password = hash;
-        let newUser = new User(userData);
-        await newUser.save();
-        resolve();
-      } catch (err) {
-        reject("There was an error encrypting the password");
-      }
+      const hash = await bcrypt.hash(userData.password, 10);
+      userData.password = hash;
+    } catch (err) {
+      reject("There was an error encrypting the password");
+    }
+    try {
+      let newUser = new User(userData);
+      await newUser.save();
+      resolve();
     } catch (err) {
       if (err.code == 11000) {
         reject("User Name already taken");
@@ -73,34 +73,30 @@ function checkUser(userData) {
       }
 
       const Match = await bcrypt.compare(userData.password, users[0].password);
-      if(!Match)
-      {
-        reject(`Incorrect Password for user: ${userData.userName}`)
+      if (!Match) {
+        reject(`Incorrect Password for user: ${userData.userName}`);
       }
 
       if (users[0].loginHistory.length == 8) {
         users[0].loginHistory.pop;
       }
 
-
       users[0].loginHistory.unshift({
         dateTime: new Date().toString(),
         userAgent: userData.userAgent,
       });
 
-
       try {
         const updateResult = await User.updateOne(
-            { userName: users[0].userName },
-            { $set: { loginHistory: users[0].loginHistory } }
-          );
-      } catch(err)  {
+          { userName: users[0].userName },
+          { $set: { loginHistory: users[0].loginHistory } }
+        );
+      } catch (err) {
         reject(`There was an error verifying the user: ${err} `);
       }
       resolve(users[0]);
-      
     } catch (err) {
-     reject(`Unable to find user: ${userData.userName}`)
+      reject(`Unable to find user: ${userData.userName}`);
     }
   });
 }
